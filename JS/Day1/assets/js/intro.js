ad8760f5ec3e5654ae77fbb9c9fd29839fe2ce0b
bala <bala>
2026-06-05 12:46:57 +0530

    updated

---
+++ JS/Day1/assets/js/intro.js
@@ -0,0 +1,27 @@
+// var 
+
+// es6
+// let 
+// const
+// variable - to store the value to the name
+
+//declaration
+// undefined
+// const userName;
+
+// console.log(userName);
+
+
+
+// 20 times - var 
+
+// var systemMonitor;
+
+// console.log(systemMonitor)
+
+
+// var cityControl;
+// 30 time let
+
+
+// 30 const
